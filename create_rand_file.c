5366778bb7b4bb7fd71e478fa4b60ca7a808a152
jalowinner <jameszylong@gmail.com>
2017-09-26 16:23:25 -0400

    Update create_rand_file.c

--- create_rand_file.c
+++ create_rand_file.c
@@ -1,1 +1,21 @@
+#include <stdlib.h>
+#include <sys/timeb.h> 
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+ 
+char c = 'A' + (rand() % 26);
+struct timeb t;
+ftime(&t);
+long now_in_ms = t.time * 1000 + t.millitm;
 
+long block_size = ...;
+char *buf = ...
+ 
+FILE *fp = fopen(filename, "w");
+fwrite(buf, 1, block_size, fp);
+fflush(fp);
+ 
+fclose(fp);
+
+void 
